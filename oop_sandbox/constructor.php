4d9f79f293ea883174d8822677a69995e1ce3239
aliahmadcse <aliahmadcse@gmail.com>
2020-03-17 21:34:00 +0500

    Adds constructor parameters

--- oop_sandbox/constructor.php
+++ oop_sandbox/constructor.php
@@ -4,36 +4,38 @@ class Sofa
 {
     public static $instance_count = 0;
 
-    public $seats = 3;
-    public $arms = 2;
+    public $seats;
+    public $arms;
 
-    public function __construct()
+    public function __construct($args = [])
     {
         self::$instance_count++;
+        $this->seats = $args['seats'] ?? NULL;
+        $this->arms = $args['arms'] ?? NULL;
     }
 }
 
 class Couch extends Sofa
 {
-    var $arms = 0;
+    // var $arms = 0;
 }
 
 class LoveSeat extends Sofa
 {
-    var $seats = 2;
+    // var $seats = 2;
 }
 
-$sofa = new Sofa();
+$sofa = new Sofa(['seats' => 3, 'arms' => 2]);
 echo $sofa->seats . "<br />";
 echo $sofa->arms . "<br />";
 echo "<hr />";
 
-$couch = new Couch();
+$couch = new Couch(['seats' => 3, 'arms' => 1]);
 echo $couch->seats . "<br/>";
 echo $couch->arms . "<br />";
 echo "<hr />";
 
-$loveseat = new LoveSeat();
+$loveseat = new LoveSeat(['seats' => 2, 'arms' => 2]);
 echo $loveseat->seats . "<br/>";
 echo $loveseat->arms . "<br />";
 echo "<hr />";
